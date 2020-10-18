bfc8f3e3048573ce6fa287a3f2703ad0da1a75a7
slawek <slawomirpawlowski@gmail.com>
2020-10-18 17:12:34 +0200

    changed plant view from genreics to ModelViewSet

--- apps/plants/views.py
+++ apps/plants/views.py
@@ -1,25 +1,19 @@
 from django.contrib.auth.models import User
 
 from rest_framework import generics, permissions
-
+from rest_framework import viewsets
 
 from .models import Plant
 from .serializers import PlantSerializer
 
 # PLANTS
-class PlantList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class PlantViewSet(viewsets.ModelViewSet):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
-
-
-class PlantDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Plant.objects.all()
-    serializer_class = PlantSerializer
 
     def perform_update(self, serializer):
         serializer.save(updated_by=self.request.user)
